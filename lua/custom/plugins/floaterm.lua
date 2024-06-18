26a3e5a7344c374de0ec8cfa8f3aa27376928c69
William M Hudson <Mitch.M.Hudson@collins.com>
2024-06-18 14:48:43 -0500

    Changed floaterm size

--- lua/custom/plugins/floaterm.lua
+++ lua/custom/plugins/floaterm.lua
@@ -4,5 +4,7 @@ return {
   config = function()
     -- Add lazygit in floating window
     vim.keymap.set('n', '<leader>og', '<cmd>FloatermNew lazygit<CR>', { desc = '[O]pen lazy[g]it' })
+    vim.g.floaterm_width = 0.9
+    vim.g.floaterm_height = 0.9
   end,
 }
